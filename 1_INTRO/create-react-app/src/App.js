2feae3077ea4898e7c20d0080934e4bb4f37eb25
Raul Lize Teixeira <lizeraul1@gmail.com>
2026-01-15 11:30:35 -0300

    feat: initialize create-react-app and vite projects with basic configurations
    Add initial project setups for both create-react-app and vite-react templates
    Include all necessary configuration files, assets, and basic components
    Set up testing, eslint, and gitignore configurations for both projects

---
+++ 1_INTRO/create-react-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='App-title'>Hello World!</h1>
+      <p className='App-text'>Welcome to React!</p>
+    </div>
+  );
+}
+
+export default App;
