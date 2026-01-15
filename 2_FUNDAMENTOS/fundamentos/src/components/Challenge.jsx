9b29cb22fd31012c63226bb8534e82ed0df8448e
Raul Lize Teixeira <lizeraul1@gmail.com>
2026-01-15 15:03:37 -0300

    style(Challenge): format button element for better readability

--- 2_FUNDAMENTOS/fundamentos/src/components/Challenge.jsx
+++ 2_FUNDAMENTOS/fundamentos/src/components/Challenge.jsx
@@ -6,7 +6,9 @@ const Challenge = () => {
         <div>
             <p>A: {a}</p>
             <p>B: {b}</p>
-            <button onClick={() => console.log(a + b)}>Clique para ver a soma!</button>
+            <button onClick={() => console.log(a + b)}>
+                Clique para ver a soma!
+            </button>
         </div>
     )
 }
