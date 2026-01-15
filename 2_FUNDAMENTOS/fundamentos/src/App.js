e9c369b7c604fe53e65fbd33860445cc240cafd7
Raul Lize Teixeira <lizeraul1@gmail.com>
2026-01-15 14:57:16 -0300

    feat(components): add Events component with event handling examples
    Introduce new Events component demonstrating various event handling patterns in React, including button click handlers and conditional rendering examples.

--- 2_FUNDAMENTOS/fundamentos/src/App.js
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -1,6 +1,8 @@
 import FirstComponent from './components/FirstComponent';
 import TemplateExpressions from './components/TemplateExpressions';
 import MyComponent from './components/MyComponent';
+import Events from './components/Events';
+import Challenge from './components/Challenge';
 import './App.css';
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <FirstComponent />
       <TemplateExpressions />
       <MyComponent />
+      <Events />
+      <Challenge />
     </div>
   );
 }
