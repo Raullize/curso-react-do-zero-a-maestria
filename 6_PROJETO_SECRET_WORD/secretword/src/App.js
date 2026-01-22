ad72be61962d48ae029ceaaedbf10c8c880eebfa
Raul Lize Teixeira <lizeraul1@gmail.com>
2026-01-21 23:34:00 -0300

    feat: add game components and stage management
    Implement Game and GameOver components with basic structure
    Add stage management logic in App.js to switch between screens

--- 6_PROJETO_SECRET_WORD/secretword/src/App.js
+++ 6_PROJETO_SECRET_WORD/secretword/src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
+
+import { useCallback, useEffect, useState} from 'react';
+
+import {wordsList} from './data/words';
+
 import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+  {id: 1, name: "start"},
+  {id: 2, name: "game"},
+  {id: 3, name: "end"},
+];
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  console.log(gameStage);
+  console.log(words);
+
   return (
     <div className="App">
-      <StartScreen />
+      {gameStage === stages[0].name && <StartScreen />}
+      {gameStage === stages[1].name && <Game />}
+      {gameStage === stages[2].name && <GameOver />}
     </div>
   );
 }
