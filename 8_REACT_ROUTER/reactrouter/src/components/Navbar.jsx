2600d163dc1cb23ebc7e71d12ba2acf9831bc1bb
Raul Lize Teixeira <lizeraul1@gmail.com>
2026-01-29 11:11:30 -0300

    feat: replace Link with NavLink for active styling
    Add conditional 'active' class to navigation links using NavLink's isActive prop. This provides visual feedback to users about the current page.

--- 8_REACT_ROUTER/reactrouter/src/components/Navbar.jsx
+++ 8_REACT_ROUTER/reactrouter/src/components/Navbar.jsx
@@ -1,13 +1,27 @@
-import './Navbar.css';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from "react-router-dom";
+
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+      {/* <Link to="/">Home</Link>
+      <Link to="/about">Sobre</Link> */}
+      {/* 8 - active link */}
+      <NavLink
+        className={({ isActive }) => (isActive ? "active" : undefined)}
+        to="/"
+      >
+        Home
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "active" : undefined)}
+        to="/about"
+      >
+        Sobre
+      </NavLink>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
